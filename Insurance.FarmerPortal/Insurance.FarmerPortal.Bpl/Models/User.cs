0b735842575c40132824d1e11f089de5e89a6a31
atuldc <atul.das13@gmail.com>
2023-04-02 23:12:27 +0530

    initial commit

---
+++ Insurance.FarmerPortal/Insurance.FarmerPortal.Bpl/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.FarmerPortal.Bpl.Models
+{
+    public class User
+    {
+
+    }
+}
---
+++ Insurance.FarmerPortal/Insurance.FarmerPortal/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Insurance.FarmerPortal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Registration")]
+        public async IAsyncResult<> UserRegister()
+        {
+
+        }
+    }
+}
