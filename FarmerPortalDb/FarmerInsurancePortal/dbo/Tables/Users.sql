b0a6c7ed3ecaad8b195e80742aff75b67ee7907b
atuldc <atul.das13@gmail.com>
2023-04-03 16:07:48 +0530

    Check in as part of database creation and integration with VS 2019 database project

---
+++ FarmerPortalDb/FarmerInsurancePortal/dbo/Tables/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [ID]          INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (50) NOT NULL,
+    [MiddleName]  VARCHAR (50) NULL,
+    [LastName]    VARCHAR (50) NOT NULL,
+    [DateOfBirth] DATE         NOT NULL,
+    [Gender]      CHAR (1)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
